d5f053b3995a32d0f0f46e794d5ffac811f92ca0
wen <zeng_wen@foxmail.com>
2021-07-18 16:04:57 +0800

    master commit2

--- src/main/java/com/wen/git/GitTest.java
+++ src/main/java/com/wen/git/GitTest.java
@@ -8,7 +8,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
-        System.out.println("hello git3!");
+        System.out.println("hello git3! master test");
         System.out.println("hello git4 by hot-fix!");
+
     }
 }
