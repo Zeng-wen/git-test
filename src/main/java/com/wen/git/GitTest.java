268d4d712c88c7fe3c4c0f2df4f5dbdd7de57043
wen <zeng_wen@foxmail.com>
2021-07-18 11:10:23 +0800

    first commit

---
+++ src/main/java/com/wen/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wen.git;
+
+/**
+ * @author wen
+ * @create 2021-07-18 11:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
